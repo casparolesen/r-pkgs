874b9a297e2244269a2b1df7d51bb04895ef40da
Caspar Olesen <cole@itu.dk>
2019-03-11 08:51:52 +0100

    Removed test files

--- script.R
+++
@@ -1,8 +0,0 @@
-install.packages("readr")
-
-library(readr)
-
-dataset <- read_csv("flygtninge.csv", skip = 5, col_names = c("Kommune", "Antal flygtninge"))
-View(dataset)
-
-save(dataset, file = "mydata.Rdata")
